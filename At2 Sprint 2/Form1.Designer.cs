1dbe5e666b27d8d4b9a0f416be6d744dcaf938b3
Tinoz9 <89823392+Tinoz9@users.noreply.github.com>
2021-10-12 11:00:31 +0800

    Add files via upload

---
+++ At2 Sprint 2/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace At2_Sprint_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.midEBtn = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.midExBox = new System.Windows.Forms.ListBox();
+            this.meanBtn = new System.Windows.Forms.Button();
+            this.modeBtn = new System.Windows.Forms.Button();
+            this.rangeBtn = new System.Windows.Forms.Button();
+            this.meanBox = new System.Windows.Forms.ListBox();
+            this.modeBox = new System.Windows.Forms.ListBox();
+            this.rangeBox = new System.Windows.Forms.ListBox();
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.seqBtn = new System.Windows.Forms.Button();
+            this.binBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.delBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.popBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // midEBtn
+            // 
+            this.midEBtn.Location = new System.Drawing.Point(316, 57);
+            this.midEBtn.Name = "midEBtn";
+            this.midEBtn.Size = new System.Drawing.Size(122, 23);
+            this.midEBtn.TabIndex = 0;
+            this.midEBtn.Text = "Mid Extreme";
+            this.midEBtn.UseVisualStyleBackColor = true;
+            this.midEBtn.Click += new System.EventHandler(this.midEBtn_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(158, 83);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(131, 329);
+            this.listBox.TabIndex = 1;
+            // 
+            // midExBox
+            // 
+            this.midExBox.FormattingEnabled = true;
+            this.midExBox.Location = new System.Drawing.Point(316, 86);
+            this.midExBox.Name = "midExBox";
+            this.midExBox.Size = new System.Drawing.Size(120, 43);
+            this.midExBox.TabIndex = 2;
+            // 
+            // meanBtn
+            // 
+            this.meanBtn.Location = new System.Drawing.Point(316, 154);
+            this.meanBtn.Name = "meanBtn";
+            this.meanBtn.Size = new System.Drawing.Size(122, 23);
+            this.meanBtn.TabIndex = 3;
+            this.meanBtn.Text = "Mean";
+            this.meanBtn.UseVisualStyleBackColor = true;
+            this.meanBtn.Click += new System.EventHandler(this.meanBtn_Click);
+            // 
+            // modeBtn
+            // 
+            this.modeBtn.Location = new System.Drawing.Point(316, 246);
+            this.modeBtn.Name = "modeBtn";
+            this.modeBtn.Size = new System.Drawing.Size(120, 23);
+            this.modeBtn.TabIndex = 4;
+            this.modeBtn.Text = "Mode";
+            this.modeBtn.UseVisualStyleBackColor = true;
+            this.modeBtn.Click += new System.EventHandler(this.modeBtn_Click);
+            // 
+            // rangeBtn
+            // 
+            this.rangeBtn.Location = new System.Drawing.Point(316, 339);
+            this.rangeBtn.Name = "rangeBtn";
+            this.rangeBtn.Size = new System.Drawing.Size(120, 23);
+            this.rangeBtn.TabIndex = 5;
+            this.rangeBtn.Text = "Range";
+            this.rangeBtn.UseVisualStyleBackColor = true;
+            this.rangeBtn.Click += new System.EventHandler(this.rangeBtn_Click);
+            // 
+            // meanBox
+            // 
+            this.meanBox.FormattingEnabled = true;
+            this.meanBox.Location = new System.Drawing.Point(316, 184);
+            this.meanBox.Name = "meanBox";
+            this.meanBox.Size = new System.Drawing.Size(120, 43);
+            this.meanBox.TabIndex = 6;
+            // 
+            // modeBox
+            // 
+            this.modeBox.FormattingEnabled = true;
+            this.modeBox.Location = new System.Drawing.Point(316, 276);
+            this.modeBox.Name = "modeBox";
+            this.modeBox.Size = new System.Drawing.Size(120, 43);
+            this.modeBox.TabIndex = 7;
+            // 
+            // rangeBox
+            // 
+            this.rangeBox.FormattingEnabled = true;
+            this.rangeBox.Location = new System.Drawing.Point(316, 369);
+            this.rangeBox.Name = "rangeBox";
+            this.rangeBox.Size = new System.Drawing.Size(120, 43);
+            this.rangeBox.TabIndex = 8;
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.Location = new System.Drawing.Point(482, 132);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(142, 23);
+            this.sortBtn.TabIndex = 9;
+            this.sortBtn.Text = "Sort";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            // 
+            // seqBtn
+            // 
+            this.seqBtn.Location = new System.Drawing.Point(482, 162);
+            this.seqBtn.Name = "seqBtn";
+            this.seqBtn.Size = new System.Drawing.Size(142, 23);
+            this.seqBtn.TabIndex = 10;
+            this.seqBtn.Text = "Search Sequential";
+            this.seqBtn.UseVisualStyleBackColor = true;
+            this.seqBtn.Click += new System.EventHandler(this.seqBtn_Click);
+            // 
+            // binBtn
+            // 
+            this.binBtn.Location = new System.Drawing.Point(482, 191);
+            this.binBtn.Name = "binBtn";
+            this.binBtn.Size = new System.Drawing.Size(142, 23);
+            this.binBtn.TabIndex = 11;
+            this.binBtn.Text = "Search Binary";
+            this.binBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(482, 249);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(142, 23);
+            this.addBtn.TabIndex = 12;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(482, 280);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(142, 23);
+            this.editBtn.TabIndex = 13;
+            this.editBtn.Text = "Edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // delBtn
+            // 
+            this.delBtn.Location = new System.Drawing.Point(482, 310);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(142, 23);
+            this.delBtn.TabIndex = 14;
+            this.delBtn.Text = "Delete";
+            this.delBtn.UseVisualStyleBackColor = true;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(158, 57);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(131, 20);
+            this.textBox.TabIndex = 15;
+            // 
+            // popBtn
+            // 
+            this.popBtn.Location = new System.Drawing.Point(482, 220);
+            this.popBtn.Name = "popBtn";
+            this.popBtn.Size = new System.Drawing.Size(142, 23);
+            this.popBtn.TabIndex = 16;
+            this.popBtn.Text = "Populate";
+            this.popBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.popBtn);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.delBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.binBtn);
+            this.Controls.Add(this.seqBtn);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.rangeBox);
+            this.Controls.Add(this.modeBox);
+            this.Controls.Add(this.meanBox);
+            this.Controls.Add(this.rangeBtn);
+            this.Controls.Add(this.modeBtn);
+            this.Controls.Add(this.meanBtn);
+            this.Controls.Add(this.midExBox);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.midEBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button midEBtn;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.ListBox midExBox;
+        private System.Windows.Forms.Button meanBtn;
+        private System.Windows.Forms.Button modeBtn;
+        private System.Windows.Forms.Button rangeBtn;
+        private System.Windows.Forms.ListBox meanBox;
+        private System.Windows.Forms.ListBox modeBox;
+        private System.Windows.Forms.ListBox rangeBox;
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.Button seqBtn;
+        private System.Windows.Forms.Button binBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button delBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button popBtn;
+    }
+}
+
---
+++ At2 Sprint 2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At2_Sprint_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int index = 0;
+        int[] hoursArray = new int[24];
+        bool sorted = false;
+
+        private void random()
+        {
+            int min = 10;
+            int max = 99;
+
+            Random randNum = new Random();
+            for (int i = 0; i < hoursArray.Length; i++)
+            {
+                if (i != null)
+                {
+                    hoursArray[i] = randNum.Next(min, max);
+                    index++;
+                    Console.WriteLine(hoursArray[i]);
+                }
+            }
+        }
+        // Method to populate array with random integers for the data stream    
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            int check = 0;
+            if (index < hoursArray.Length)
+            {
+                if (int.TryParse(textBox.Text, out check))
+                {
+                    hoursArray[index] = check;
+                    index++;
+                    textBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please enter an integer");
+                }
+            }
+            display();
+        }
+        // Method that allows user to add elements to the array 
+        // Checks to see if there is an integer entered
+        // Message box to display error if otherwise
+
+        private void display()
+        {
+            try
+            {
+                listBox.Items.Clear();
+                for (int i = 0; i < index; i++)
+                {
+                    listBox.Items.Add(hoursArray[i]);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Array is full");
+            }
+        }
+        // Method displays the array elements in the listbox 
+        // Error handing to check if array is full 
+
+        private void editBtn_Click(object sender, EventArgs e)
+        {
+            int b;
+            int a = listBox.SelectedIndex;
+            listBox.Items.RemoveAt(a);
+            bool c = int.TryParse(textBox.Text, out b);
+            if (c == true)
+            {
+                listBox.Items.Insert(a, b);
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer");
+            }
+        }
+        // Method to allow user to select and edit items in the listbox (array)
+        // Error handling to check if input is an integer
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            hoursArray[listBox.SelectedIndex] = hoursArray[index - 1];
+            index--;
+            display();
+        }
+        // Method to delete selected item from the listbox (array)  
+
+        private void sortBtn_Click(object sender, EventArgs e)
+        {
+            sort();
+            display();
+        }
+        // Sort button that uses the bubble sort algorithm
+
+        private void binBtn_Click(object sender, EventArgs e)
+        {
+            int target;
+            bool b = int.TryParse(textBox.Text, out target);
+            int high = index - 1;
+            int low = 0;
+            int i = target;
+
+            if (sorted)
+            {
+                try
+                {
+                    while (low <= high)
+                    {
+                        int mid = (high + low) / 2;
+                        if (target == hoursArray[mid])
+                        {
+                            MessageBox.Show("Target found");
+                            break;
+                        }
+                        else if (target > hoursArray[mid])
+                        {
+                            low = mid + 1;
+                        }
+                        else
+                        {
+                            high = mid - 1;
+                        }
+                        if (b == false)
+                        {
+                            MessageBox.Show("Please enter an integer");
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Target not found");
+                    throw;
+                }
+            }
+        }
+        // Binary search method to search for a user inputted value in the listbox (array)
+        // Error handling to ensure the list is sorted before binary search
+
+        private void sort()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                for (int k = 0; k < index - 1; k++)
+                {
+                    if (hoursArray[k] > hoursArray[k + 1])
+                    {
+                        int temp = hoursArray[k];
+                        hoursArray[k] = hoursArray[k + 1];
+                        hoursArray[k + 1] = temp;
+                    }
+                }
+            }
+            sorted = true;
+        }
+        // Method to sort items in the listbox (Hoursarray) using bubble sort 
+        // Set sorted to true for error handling to ensure list is sorted before binary search 
+
+        private void buttonPop_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            random();
+            display();
+        }
+
+        private void midEBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void meanBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rangeBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seqBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Populate button to add the random integers in the array to the list 
+
+    }
+}
